8c491bef3b95ca072715bc18ad0f65d48dfac047
Sandeep <skrudra12@gmail.com>
2025-10-24 12:18:37 -0500

    1. Method reference Lambda on Custom Object

---
+++ src/CustomObjectFilterFunctional.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class CustomObjectFilterFunctional {
+
+    public static void main(String[] args) {
+
+        List<Course> courses = new ArrayList<>();
+        courses.add(new Course("Java Programming"));
+        courses.add(new Course("C Programming"));
+        courses.add(new Course("C++ Programming"));
+        courses.add(new Course("C# Programming"));
+        courses.add(new Course("Spring"));
+        courses.add(new Course("Spring Boot"));
+
+        // print all the courses
+        courses.forEach(System.out::println);
+
+        // print the courses that starts with C
+        courses.stream()
+                .filter(course -> course.name().startsWith("C"))
+                .forEach(System.out::println);
+
+        // print the courses that has Spring in their name
+        courses.stream()
+                .filter(course -> course.name().contains("Spring"))
+                .forEach(System.out::println);
+    }
+}
+
+
+record Course(String name) {
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
