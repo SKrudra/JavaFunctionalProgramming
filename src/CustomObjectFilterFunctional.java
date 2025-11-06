5fef46eafbcad78f158d2b1ad72490d79714fb90
Sandeep <skrudra12@gmail.com>
2025-11-06 14:55:56 -0600

    1. anyMatch, allMatch, and noneMatch operations on streams

--- src/CustomObjectFilterFunctional.java
+++ src/CustomObjectFilterFunctional.java
@@ -1,18 +1,19 @@
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+import java.util.function.Predicate;
 
 public class CustomObjectFilterFunctional {
 
     public static void main(String[] args) {
 
         List<Course> courses = new ArrayList<>();
-        courses.add(new Course("Java Programming"));
-        courses.add(new Course("C Programming"));
-        courses.add(new Course("C++ Programming"));
-        courses.add(new Course("C# Programming"));
-        courses.add(new Course("Spring"));
-        courses.add(new Course("Spring Boot"));
+        courses.add(new Course("Java Programming", 4));
+        courses.add(new Course("C Programming", 5));
+        courses.add(new Course("C++ Programming", 4));
+        courses.add(new Course("C# Programming", 5));
+        courses.add(new Course("Spring", 4));
+        courses.add(new Course("Spring Boot", 5));
 
         // print all the courses
         courses.forEach(System.out::println);
@@ -46,11 +47,25 @@ public class CustomObjectFilterFunctional {
         courses.stream()
                 .sorted(Comparator.comparing(c -> c.name().length()))
                 .forEach(System.out::println);
+
+        Predicate<Course> greaterThan4Ratings = course -> course.ratings() > 4;
+
+        // allMatch
+        System.out.println("All ratings greater than 4 " + courses.stream()
+                .allMatch(greaterThan4Ratings));
+
+        // anyMatch
+        System.out.println("Any course with rating greater than 4 " + courses.stream().anyMatch(greaterThan4Ratings));
+
+        // noneMatch
+        System.out.println("No course with rating greater than 4 " + courses.stream().noneMatch(greaterThan4Ratings));
     }
+
+
 }
 
 
-record Course(String name) {
+record Course(String name, int ratings) {
 
     @Override
     public String toString() {
